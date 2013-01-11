14f2f2ed5b90cd5cfb2f88e2debab6d623ab09ea
Tom Ashworth <tom@phuu.net>
2013-01-11 00:26:15 +0000

    Nicer formatting.

--- distra.js
+++ distra.js
@@ -4,10 +4,7 @@ var connect = require('connect')
   , path = require('path')
   , config = require('./config.json')
   , static_server = connect()
-  , proxy_options = {
-      hostnameOnly: true,
-      router: {}
-    }
+  , proxy_options = { hostnameOnly: true, router: {} }
   , port = {
       proxy: process.argv[2] || process.env.PORT || 8000,
       static_server: 9999
@@ -25,10 +22,14 @@ Object.keys(config).forEach(function (host) {
   }
 });
 
-http_proxy.createServer(proxy_options).listen(port.proxy, function () {
-  console.log('proxy listening on %d', port.proxy);
-});
+http_proxy
+  .createServer(proxy_options)
+  .listen(port.proxy, function () {
+    console.log('proxy listening on %d', port.proxy);
+  });
 
-http.createServer(static_server).listen(port.static_server, function () {
-  console.log('static listening on %d', port.static_server);
-});
+http
+  .createServer(static_server)
+  .listen(port.static_server, function () {
+    console.log('static listening on %d', port.static_server);
+  });
